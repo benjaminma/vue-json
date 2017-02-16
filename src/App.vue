0476ca1f983eed5fa84c303f0d719b4f154f4f54
Ben Ma <git.app@benjaminma.com>
2017-02-15 21:38:43 -0800

    HACK: Force error snackbar bgcolor to be red

--- src/App.vue
+++ src/App.vue
@@ -18,10 +18,15 @@ export default {
 
 <style lang="scss">
 $font-mono: 'Monaco', Courier, monospace;
+$color-darkred: darkred;
 
 #app {
   font-family: $font-mono;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
+}
+
+.md-snackbar-container {
+  background: $color-darkred !important;
 }
 </style>
